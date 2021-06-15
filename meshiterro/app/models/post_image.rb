2f23176fd9186dca8274a8c70ac6b69e6da5fbfa
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-15 11:19:06 +0000

    4finish

--- meshiterro/app/models/post_image.rb
+++ meshiterro/app/models/post_image.rb
@@ -3,4 +3,11 @@ class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image #_idは含めません
   has_many :post_comments,dependent: :destroy
+  has_many :favorites,dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
+
 end

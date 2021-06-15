0dc3ed2c5ee5a6a376b3b1c09461ebc4f3c7653f
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-15 07:54:45 +0000

    continued1

--- meshiterro/app/models/post_image.rb
+++ meshiterro/app/models/post_image.rb
@@ -1,5 +1,6 @@
 class PostImage < ApplicationRecord
-  
+
   belongs_to :user
   attachment :image #_idは含めません
+  has_many :post_comments,dependent: :destroy
 end
---
+++ meshiterro/test/controllers/post_commnets_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PostCommnetsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
